1639b57b553772ff6b1070367946155532a05306
Abhishek mohata <36244104+amohata007@users.noreply.github.com>
2022-10-26 17:01:37 +0530

    Create MergeSort.js

---
+++ MergeSort.js
@@ -0,0 +1,14 @@
+function merge(left, right) {
+  let sortedArr = [] // the sorted items will go here
+  while (left.length && right.length) {
+    // Insert the smallest item into sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift())
+    } else {
+      sortedArr.push(right.shift())
+    }
+  }
+  // Use spread operators to create a new array, combining the three arrays
+  return [...sortedArr, ...left, ...right]
+}
+merge([1, 4], [2, 6, 9]) // [1, 2, 4, 6, 9]
